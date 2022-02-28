06f7c732a798334fa6fd2da6c860bc2836f7f25e
Priscila Passos <82420437+Priscylla-Passos@users.noreply.github.com>
2022-02-27 23:24:39 -0300

    fimLayout

--- Flutter/Exercicico Layout/layout/lib/main.dart
+++ Flutter/Exercicico Layout/layout/lib/main.dart
@@ -1,26 +1,28 @@
 import 'package:flutter/material.dart';
 
 import 'exercicio5/card_nft.dart';
-//import 'package:layout/exercicio1/appbar.dart';
-//import 'package:layout/exercicio2/drawer.dart';
-// import 'package:layout/exercicio3/navigation_bar.dart';
-// import 'package:layout/exercicio4/progress_bar.dart';
+import 'package:layout/exercicio1/appbar.dart';
+import 'package:layout/exercicio2/drawer.dart';
+import 'package:layout/exercicio3/navigation_bar.dart';
+import 'package:layout/exercicio4/progress_bar.dart';
 
 //Exercicioc1
-// void main() {
+//   void main() {
 //   runApp(const MeuAppBar());
 // }
 //Exercicio 2
-//
-//Exercicio 3
 // void main() {
-//   runApp(const MyNavigatorBar());
+//   runApp(const MenuDrawer());
 // }
+//Exercicio 3
+ void main() {
+   runApp(const MyNavigatorBar());
+ }
 //Exercicio 4
 // void main() {
 //   runApp(const ProgressBar());
 // }
 //Exercicio 5
-void main() {
-  runApp(const CardNFT());
-}
+// void main() {
+//   runApp(const CardNFT());
+// }
